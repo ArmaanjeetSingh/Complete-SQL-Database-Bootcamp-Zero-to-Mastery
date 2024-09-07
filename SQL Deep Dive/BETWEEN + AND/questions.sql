1044f691c99ad09e2a5a89e2e5432968963a524b
ArmaanjeetSingh <162802779+ArmaanjeetSingh@users.noreply.github.com>
2024-09-06 22:07:57 -0700

    Update questions.sql

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -1,11 +1,8 @@
 -- Who between the ages of 30 and 50 has an income less than 50 000?
 -- (include 30 and 50 in the results)
 
-/*
-* Write your query here
-*/
+SELECT * FROM customers where (age BETWEEN 30 and 50) and income < 50000;
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
-/*
-* Write your query here
-*/
+
+SELECT avg( income ) from public.customers WHERE age BETWEEN 20 and 30;
