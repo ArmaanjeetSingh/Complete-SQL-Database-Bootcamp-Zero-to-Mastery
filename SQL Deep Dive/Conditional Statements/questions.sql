652c9bccf64e176cb029fca1b93ed5b287565d0e
ArmaanjeetSingh <162802779+ArmaanjeetSingh@users.noreply.github.com>
2024-10-01 20:41:18 -0700

    Update questions.sql

--- SQL Deep Dive/Conditional Statements/questions.sql
+++ SQL Deep Dive/Conditional Statements/questions.sql
@@ -5,3 +5,12 @@
 * if it's between 10 and 20 you show 'average' 
 * and of is lower than or equal to 10 you show 'cheap'.
 */
+ SELECT 
+ p.prod_id,p.title,p.price,
+ CASE 
+    WHEN p.price > 20 THEN 'expensive'
+    WHEN p.price <=10 tHEN 'cheap'
+    WHEN price BETWEEN 10 and 20 THEN 'average'
+    END as "price class"
+   
+ from public.products as p
